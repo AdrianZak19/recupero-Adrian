310203994c4610fdcb5f6d1cb5fd3d1a2dbb9eb8
AdrianZak19 <zak8552@gmail.com>
2023-03-29 19:19:59 +0200

    es1.py

--- es1.py
+++ es1.py
@@ -1,1 +1,21 @@
-print('ciao')
+global nome
+nome:str='a'
+cognome:str='a'
+o=int(input('inserire numero persone '))
+
+def Lista():
+    i=0
+    l=[]
+    for x in range(o):
+        a=str(input('nome: '))
+        b=str(input('cognome: '))
+        l.append(a+ " " +b)
+        if nome==a and cognome==b:
+            i+=1
+
+    return l, i
+
+def main():
+    lista=Lista()
+    print(lista)
+main()
